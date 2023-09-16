5a6edfd79a14ac53df5ddde82b1a387e615195fb
Sam Keamy <60070276+AnnoyingDoge@users.noreply.github.com>
2023-09-16 18:16:26 -0400

    moved more methods to C# from lua, more UI

--- CSharpTool/src/CSharpToolForm.cs
+++ CSharpTool/src/CSharpToolForm.cs
@@ -22,37 +22,107 @@ namespace Net.MyStuff.CSharpTool
         private ApiContainer APIs
             => _maybeAPIContainer!;
 
+        //button to restart (load save state 1)
+        Button restartButton = new Button() { AutoSize = true, Text = "Restart" };
+
+        Button startJump = new Button() { AutoSize = true, Text = "Hold Jump", Location = new Point(50, 50) };
+        Button startRight = new Button() { AutoSize = true, Text = "Hold Right", Location = new Point(100, 50) };
+
+
+
+
         public CSharpToolForm()
         {
+            //add EventHandler to Click action, calls method on click
+            restartButton.Click += new EventHandler(RestartButton_Click);
+            startJump.Click += new EventHandler(StartJump_Click);
+            startRight.Click += new EventHandler(HoldRight_Click);
+
+
+            //create tool window
             ClientSize = new Size(480, 320);
             SuspendLayout();
-            Controls.Add(new Label { AutoSize = true, Text = "Hello, world!" });
+            Controls.Add(restartButton);
+            Controls.Add(startJump);
+            Controls.Add(startRight);
             ResumeLayout(performLayout: false);
             PerformLayout();
         }
 
+        /// <summary>
+        /// load save state 1 on restart button click
+        /// </summary>
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            loadSaveState();
+        }
+
+        private void StartJump_Click(object sender, EventArgs e)
+        {
+            holdingJump(true);
+        }
+
+        private void HoldRight_Click(object sender, EventArgs e)
+        {
+            holdingRight(true);
+        }
+
+        /// <summary>
+        /// Get position of camera in level
+        /// </summary>
+        /// <returns>Camera postiion as unit</returns>
         public uint GetCameraPosition()
         {
+            APIs.Memory.UseMemoryDomain(APIs.Memory.MainMemoryName); //position information stored in main RAM of game
+            APIs.Memory.SetBigEndian(true); //position information uses big Endian
             return APIs.Memory.ReadU16(0xEE00);
         }
+
+
+        public void DrawGUIElements()
+        {
+            APIs.Gui.Text(50, 75, "Position: " + position);
+            APIs.Gui.Text(50, 50, "Fitness : " + fitness);
+        }
+
+        /// <summary>
+        /// load save state of level beginning (training starts here)
+        /// </summary>
+        public void loadSaveState()
+        {
+            APIs.SaveState.LoadSlot(1);
+        }
+
+
+        #region input
+
+        public void holdingJump(bool holding)
+        {
+            APIs.Joypad.Set("A", holding, 1);
+        }
+        public void holdingRight(bool holding)
+        {
+            APIs.Joypad.Set("Right", holding, 1);
+        }
+
+        #endregion
 
         public override void Restart()
         {
             // executed once after the constructor, and again every time a rom is loaded or reloaded
         }
 
+        // executed after every frame (except while turboing, use FastUpdateAfter for that)
         protected override void UpdateAfter()
         {
-            APIs.Memory.UseMemoryDomain(APIs.Memory.MainMemoryName);
             position = (int)GetCameraPosition();
 
-            APIs.Memory.SetBigEndian(true);
-            APIs.Gui.Text(50, 150, "Position: " + position);
-            APIs.Gui.Text(50, 100, "Position2: " + APIs.Memory.ReadS16(0xEE00));
-            APIs.Gui.Text(50, 50, "Position3: " + APIs.Memory.ReadFloat(0xEE00));
+            //fitness increases when position is greater than fitness, i.e. fitness score = furthest position
+            if (position > fitness)
+                fitness = position;
 
-
-            // executed after every frame (except while turboing, use FastUpdateAfter for that)
+            //Draw GUI stuff so we can see what's going on
+            DrawGUIElements();   
         }
     }
 }
